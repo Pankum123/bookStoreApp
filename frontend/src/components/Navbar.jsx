3cbd9560e11b9f05df0086293dd65748d01a85ad
Pankaj <pka936636@gmail.com>
2025-09-09 02:15:15 +0530

    link set

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Login from "./Login";
 import Logout from "./Logout";
 import { useAuth } from "../context/AuthProvider"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [authUser, setAuthUser] = useAuth();
@@ -41,18 +42,10 @@ const Navbar = () => {
 
   const navItems = (
     <>
-      <li>
-        <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="course">Course</a>
-      </li>
-      <li>
-        <a href="contact">Contact</a>
-      </li>
-      <li>
-        <a href="about">About</a>
-      </li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/course">Course</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+      <li><Link to="/about">About</Link></li>
     </>
   );
 
